716e7cfaf17776b89c2fe378526a292929fcba3c
Theo White <Theowhite4@gmail.com>
2021-02-10 20:24:00 -0600

    Commit to implement read/write on csv file

---
+++ com/theoscode/csv.java
@@ -0,0 +1,54 @@
+package com.theoscode;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Reader;
+import java.io.Writer;
+import java.util.ArrayList;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVPrinter;
+import org.apache.commons.csv.CSVRecord;
+import com.theoscode.csvData;
+/*
+ * @author Theo White
+ * 
+ */
+
+public class csv {
+
+	static String fileName = "data.csv";
+	static ArrayList<csvData> csvDataArrayList = new ArrayList<csvData>();
+	
+	/*
+	 * @param used to write the cvsDataArrayList to a file
+	 */
+	public static void writeData(ArrayList<csvData> csvDataToWrite) throws IOException {
+		String[] data = new String[] { "product_id", "quantity", "wholesale_cost", "sale_price", "supplier_id" };
+		Writer writer = new FileWriter(fileName);
+		CSVPrinter csvPrinter = new CSVPrinter(writer, CSVFormat.DEFAULT.withHeader(data));
+		for (csvData record : csvDataToWrite) {
+			csvPrinter.printRecord(record.product_id,record.quantity,record.wholesale_cost,record.sale_price,record.supplier_id);
+		}
+	}
+
+	/*
+	 * @param used to read data for the given data file
+	 */
+	public static void readData() throws IOException {
+		try (Reader reader = new FileReader(fileName);
+				CSVParser csvParser = new CSVParser(reader,
+						CSVFormat.DEFAULT.withFirstRecordAsHeader().withIgnoreHeaderCase().withTrim())) {
+			for (CSVRecord record : csvParser) {
+				String product_id = record.get("product_id");
+				String quantity = record.get("quantity");
+				String wholesale_cost = record.get("wholesale_cost");
+				String sale_price = record.get("sale_price");
+				String supplier_id = record.get("supplier_id");
+				csvDataArrayList.add(new csvData(product_id, quantity, wholesale_cost, sale_price, supplier_id));
+
+			}
+		}
+	}
+}
---
+++ com/theoscode/csvData.java
@@ -0,0 +1,63 @@
+/*
+ * @author Theo White
+ * 
+ */
+
+public class csvData {
+
+	String product_id;
+	String quantity;
+	String wholesale_cost;
+	String sale_price;
+	String supplier_id;
+
+	public csvData(String product_id, String quantity, String wholesale_cost, String sale_price, String supplier_id) {
+		super();
+		this.product_id = product_id;
+		this.quantity = quantity;
+		this.wholesale_cost = wholesale_cost;
+		this.sale_price = sale_price;
+		this.supplier_id = supplier_id;
+	}
+
+	public String getProduct_id() {
+		return product_id;
+	}
+
+	public void setProduct_id(String product_id) {
+		this.product_id = product_id;
+	}
+
+	public String getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(String quantity) {
+		this.quantity = quantity;
+	}
+
+	public String getWholesale_cost() {
+		return wholesale_cost;
+	}
+
+	public void setWholesale_cost(String wholesale_cost) {
+		this.wholesale_cost = wholesale_cost;
+	}
+
+	public String getSale_price() {
+		return sale_price;
+	}
+
+	public void setSale_price(String sale_price) {
+		this.sale_price = sale_price;
+	}
+
+	public String getSupplier_id() {
+		return supplier_id;
+	}
+
+	public void setSupplier_id(String supplier_id) {
+		this.supplier_id = supplier_id;
+	}
+
+}
