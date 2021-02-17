8e9095425b2e3c1309ed4154fd4acfe5c437b86a
Rory <roguma@msudenver.edu>
2021-02-16 22:42:52 -0700

    Changes to update and main
    Added looping logic to update.
    Refactored user request in main.
    Sorry this was so late.

--- src/main/java/com/pinkpythons/csv/crud/read/Update.java
+++ src/main/java/com/pinkpythons/csv/crud/read/Update.java
@@ -1,4 +1,20 @@
 package com.pinkpythons.csv.crud.read;
 
+import com.pinkpythons.csv.CSV;
+import com.pinkpythons.csv.CsvData;
+
+
 public class Update {
+
+    public static void question(String desiredID, String quantity, String wholesaleCost, String salePrice, String supplierID) {
+
+        for (CsvData data : CSV.CsvDataArrayList) {
+            if (data.getProduct_id() == desiredID) {
+                data.setWholesale_cost(wholesaleCost);
+                data.setSupplier_id(supplierID);
+                data.setSale_price(salePrice);
+                data.setQuantity(quantity);
+            }
+        }
+    }
 }
